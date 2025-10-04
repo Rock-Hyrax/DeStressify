78985436bd653a027835068d35f4b07c762199a8
mwawrzen <w.w@op.pl>
2025-10-04 13:31:06 +0200

    feat: add sample chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { BarChart } from "@mui/x-charts/BarChart";
 import "./App.css";
 
 const App= ()=> {
   return (
-    <div className="app">
-      
+    <div>
+      <BarChart
+        xAxis={[{ data: ['group A', 'group B', 'group C'] }]}
+        series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+        height={300}
+      />
     </div>
   );
 }
