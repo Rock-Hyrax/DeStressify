02dd5ae6262df7d00b6d6c5c8459ee9e6e7bd573
jaanonim <mat8mro@gmail.com>
2025-10-05 00:49:04 +0200

    ipc

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,8 +1,11 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+  onReport: (callback) => ipcRenderer.on('report', (_event, value) => callback(value)),
+  getRange: (start, end) => ipcRenderer.send('getRange', start, end)
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,24 +1,27 @@
-import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
-import { BrowserRouter } from "react-router";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import App from "./App";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import App from './App'
+import './index.css'
 
-const theme= createTheme({
-  palette: { mode: "dark" }
-});
+const theme = createTheme({
+  palette: { mode: 'dark' }
+})
 
-const root= createRoot(
-  document.getElementById( "root" ) as HTMLElement
-);
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+window.api.getRange(Date.now() - 3600 * 1000, Date.now())
+window.api.onReport((value) => {
+  console.log(value)
+})
 
 root.render(
   <StrictMode>
-    <ThemeProvider theme={ theme }>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
-);
+)
