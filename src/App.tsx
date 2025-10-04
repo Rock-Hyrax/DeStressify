88a27881ccf30607284456d8817abf75fe01785b
mwawrzen <w.w@op.pl>
2025-10-04 19:27:43 +0200

    feat: update menu (settings)

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import HomePage from "./Pages/Home/Home";
 import StressLevelPage from "./Pages/StressLevel/StressLevel";
 import ApplicationsPage from "./Pages/Applications/Applications";
 import AnalyticsPage from "./Pages/Analytics/Analytics";
+import SettingsPage from "./Pages/Settings/Settings";
 import Menu from "./Components/Menu/Menu.component";
-import "./App.css";
 import NotFoundPage from "./Pages/NotFound";
+import "./App.css";
 
 const App= ()=> {
   return (
@@ -17,6 +18,7 @@ const App= ()=> {
           <Route path="/stress-level" element={ <StressLevelPage /> } />
           <Route path="/applications" element={ <ApplicationsPage /> } />
           <Route path="/analytics" element={ <AnalyticsPage /> } />
+          <Route path="/settings" element={ <SettingsPage /> } />
           <Route path="*" element={ <NotFoundPage /> } />
         </Routes>
       </main>
--- src/Components/Menu/Menu.component.tsx
+++ src/Components/Menu/Menu.component.tsx
@@ -3,6 +3,7 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import ShowChartIcon from '@mui/icons-material/ShowChart';
 import GridViewIcon from '@mui/icons-material/GridView';
 import BarChartIcon from '@mui/icons-material/BarChart';
+import SettingsIcon from '@mui/icons-material/Settings';
 import logo from "../../assets/logo/logo.png";
 
 import "./Menu.css";
@@ -22,6 +23,7 @@ const Menu= ()=> {
           <NavLink to="/stress-level"><ShowChartIcon />Stress level</NavLink>
           <NavLink to="/applications"><GridViewIcon />Applications</NavLink>
           <NavLink to="/analytics"><BarChartIcon />Analytics</NavLink>
+          <NavLink to="/settings"><SettingsIcon />Settings</NavLink>
         </ul>
       </nav>
     </div>
