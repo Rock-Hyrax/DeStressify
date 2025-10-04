85f885ab076f7e9a2c1c7d18b772981963b89ff2
mwawrzen <w.w@op.pl>
2025-10-05 00:21:22 +0200

    feat: update data object type

---
+++ src/utilities/types.ts
@@ -0,0 +1,23 @@
+type AppTimePerAppObject= any;
+
+export type DataObject= {
+  SnLeftClicked: number,
+  SnMouseAct: number,
+  SnKeyStrokes:number,
+  SnChars:number,
+  SnSpecialKeys:number,
+  SnDirectionKeys:number,
+  SnSpaces:number,
+  SnDoubleClicked: number,
+  SnShortCutKeys:number,
+  SnWheel:number,
+  SnErrorKeys:number,
+  SnRightClicked: number,
+  CharactersRatio: number,
+  ErrorKeyRatio: number,
+  SnMouseDistance: number,
+  SnAppChange: number,
+  AppTimePerApp: AppTimePerAppObject,
+  Stress: number,
+  timestamp: string
+};
