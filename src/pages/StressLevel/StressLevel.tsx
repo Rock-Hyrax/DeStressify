4eea35e608206da15012c19575c4dbfda8de996c
mwawrzen <w.w@op.pl>
2025-10-04 20:36:14 +0200

    feat: remove testing data

--- src/pages/StressLevel/StressLevel.tsx
+++ src/pages/StressLevel/StressLevel.tsx
@@ -1,19 +1,7 @@
 import { LineChart } from '@mui/x-charts/LineChart';
 import "./StressLevel.css";
 
-const xs: any= [];
-const ys: any= [];
-let a = 0
-for( let i=0 ; i< 100; i++ ) {
-  xs.push( a += Math.random());
-  ys.push( Math.floor( Math.random()* 10 ));
-}
-
 const StressLevelPage= ()=> {
-
-  console.log( xs, ys );
-
-  // xs.sort();
 
   return (
     <LineChart
