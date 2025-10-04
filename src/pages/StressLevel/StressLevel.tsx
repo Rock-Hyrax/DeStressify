d6394290a2206e8a8751474798329a525e427e43
mwawrzen <w.w@op.pl>
2025-10-04 20:35:50 +0200

    feat: add sample chart

--- src/pages/StressLevel/StressLevel.tsx
+++ src/pages/StressLevel/StressLevel.tsx
@@ -1,8 +1,31 @@
+import { LineChart } from '@mui/x-charts/LineChart';
 import "./StressLevel.css";
 
+const xs: any= [];
+const ys: any= [];
+let a = 0
+for( let i=0 ; i< 100; i++ ) {
+  xs.push( a += Math.random());
+  ys.push( Math.floor( Math.random()* 10 ));
+}
+
 const StressLevelPage= ()=> {
+
+  console.log( xs, ys );
+
+  // xs.sort();
+
   return (
-    <p>stress level</p>
+    <LineChart
+      xAxis={[{ data: [ 1, 2, 3, 4, 5, 6 ]}]}
+      series={[
+        {
+          data: [ 1, 2, 3, 4, 5, 6 ],
+          showMark: false
+        }
+      ]}
+      height={300}
+    />
   );
 };
 
