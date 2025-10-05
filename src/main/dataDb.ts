3d082b22be100ac49b532f9dc7475d868e55c582
n2one <dominikwojtasik191@gmail.com>
2025-10-05 05:12:27 +0200

    stage 1

--- src/main/dataDb.ts
+++ src/main/dataDb.ts
@@ -1,11 +1,11 @@
 import * as path from "path";
-import { app } from "electron";
-import { JSONFilePreset } from "lowdb/node";
+import {app} from "electron";
+import {JSONFilePreset} from "lowdb/node";
 
 const userDataPath = app.getPath("userData");
 const filePath = path.join(userDataPath, "db.json");
 console.log(filePath);
-const defaultData = { reports: [] } as any;
+const defaultData = {reports: []} as any;
 
 const getDb = async () => {
   const db = await JSONFilePreset(filePath, defaultData);
@@ -14,7 +14,7 @@ const getDb = async () => {
 
 export const addReport = async (report: any) => {
   const db = await getDb();
-  await db.update(({ reports }) => reports.push(report));
+  await db.update(({reports}) => reports.push(report));
 };
 
 export const getReports = async () => {
@@ -24,6 +24,38 @@ export const getReports = async () => {
 };
 
 export const getRange = async (start: number, end: number) => {
-  const data = await getReports();
-  return data.filter((ele) => ele.Timestamp > start && ele.Timestamp < end);
+  let data = await getReports();
+  data = data.filter((ele) => ele.Timestamp > start && ele.Timestamp < end);
+  const movingAverage: any[] = [];
+  const windowSize = 5;
+
+  for (let i = 0; i < data.length; i++) {
+    // Wyznacz start okna - max 0 lub i-4, żeby nie wyjść poza początek tablicy
+    const start = Math.max(0, i - windowSize + 1);
+    const window = data.slice(start, i + 1); // Pobierz wartości w oknie
+    const sum = window.reduce((acc, val) => {
+      console.log(acc, val)
+      Object.keys(val).forEach((key) => {
+        if (key === "Timestamp" || typeof val[key] !== "number") {
+          acc = val[key]
+        }
+        if (acc[key] !== undefined) {
+          acc[key] += val[key];
+        } else {
+          acc[key] = val[key];
+        }
+      })
+      return acc;
+    }, {}); // Suma wartości w oknie
+    const avg = {}
+    Object.keys(sum).forEach((key) => {
+      if (key === "Timestamp" && typeof sum[key] !== "number") {
+        avg[key] = sum[key]
+      }
+      avg[key] = sum[key] / window.length
+    })
+    movingAverage.push(avg); // Dodaj do nowej tablicy
+  }
+  return movingAverage;
+
 };
