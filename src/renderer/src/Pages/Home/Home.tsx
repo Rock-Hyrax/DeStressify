05178a39b3a7153b4beca9674a44d44509287fed
jaanonim <mat8mro@gmail.com>
2025-10-05 03:01:13 +0200

    Add TopAppsWidget component to display most used apps today

--- src/renderer/src/Pages/Home/Home.tsx
+++ src/renderer/src/Pages/Home/Home.tsx
@@ -1,15 +1,16 @@
+import TopAppsWidget from "@renderer/Components/Widgets/TopApps";
 import StressLevelWidget from "../../Components/Widgets/StressLevel";
 import StressLevelTodayWidget from "../../Components/Widgets/StressLevelToday";
 import WorkTimeWidget from "../../Components/Widgets/WorkTime/WorkTime.component";
 import "./Home.css";
 
-const HomePage= ()=> {
-
+const HomePage = () => {
   return (
     <div className="home_container">
       <StressLevelWidget />
       <StressLevelTodayWidget />
       <WorkTimeWidget />
+      <TopAppsWidget />
     </div>
   );
 };
